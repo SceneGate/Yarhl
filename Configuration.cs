1cb68f542a39a5edf78f7ecef3748425796dc786
pleonex <benito356@gmail.com>
2013-09-22 02:02:44 +0200

    Refactoring. Configuration and RescueFile moved to libgame RescueFile method will be needed by some plugins (formats) to get files that can not be determined as dependency (ie: path writting in its data)
    Configuration xml file has been merged to xml Edit since it
    contains the configuration of the current game modification.
    It has been moved to libgame to be accesible by plugins.
    
    * Configuration.cs: Fixed bug getting Nitro.Rom format
    * Worker.cs: Fixed bug not resolving path at Import method

---
+++ Configuration.cs
@@ -0,0 +1,139 @@
+//-----------------------------------------------------------------------
+// <copyright file="Configuration.cs" company="none">
+// Copyright (C) 2013
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by 
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful, 
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details. 
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see "http://www.gnu.org/licenses/". 
+// </copyright>
+// <author>pleoNeX</author>
+// <email>benito356@gmail.com</email>
+// <date>12/06/2013</date>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace Libgame
+{
+	public class Configuration
+	{
+		private static Configuration Instance;
+
+		private XDocument xmlEdit;
+		private Dictionary<string, string> relativePaths;
+		private Dictionary<string, char[,]> tables;
+		private string ellipsis;
+		private char[] quotes;
+
+		private Configuration(XDocument xmlEdit)
+		{
+			this.xmlEdit = xmlEdit;
+			this.ReadConfig();
+		}
+
+		public XElement XEdit {
+			get { return this.xmlEdit.Root; }
+		}
+
+		public ReadOnlyDictionary<string, char[,]> Tables {
+			get { return new ReadOnlyDictionary<string, char[,]>(this.tables); }
+		}
+
+		public string Ellipsis {
+			get { return this.ellipsis; }
+		}
+
+		public char[] Quotes {
+			get { return this.quotes; }
+		}
+
+		public static Configuration GetInstance()
+		{
+			if (Instance == null)
+				throw new Exception("The class has not been initialized.");
+			return Instance;
+		}
+
+		public static void Initialize(XDocument xmlEdit)
+		{
+			Instance = new Configuration(xmlEdit);
+		}
+
+		public string ResolvePath(string path)
+		{
+			if (!path.Contains("{$") || string.IsNullOrEmpty(path))
+				return path;
+
+			int pos = path.IndexOf('{');
+			while (pos != -1) {
+				// Get variable
+				string variable = path.Substring(pos + 2, path.IndexOf('}', pos) - (pos + 2));
+				path = path.Replace("{$" + variable + "}", this.relativePaths[variable]);
+
+				pos = path.IndexOf('{', pos);
+			}
+
+			return path;
+		}
+
+		private void ReadConfig()
+		{
+			XElement root = this.xmlEdit.Root;
+
+			// Get relative paths
+			this.relativePaths = new Dictionary<string, string>();
+			foreach (XElement xrel in root.Element("RelativePaths").Elements("Path")) {
+				string variable = xrel.Element("Variable").Value;
+				string path = xrel.Element("Location").Value;
+
+				path = ResolvePath(path);
+				this.relativePaths.Add(variable, path);
+			}
+
+			// FUTURE: What about adding a "Encoding" field?
+
+			// Get tables
+			this.tables = new Dictionary<string, char[,]>();
+			foreach (XElement xtbl in root.Element("CharTables").Elements("Table")) {
+				string name = xtbl.Attribute("name").Value;
+				char[,] table = new char[xtbl.Elements().Count(), 2];	// Chars to replace: original <-> new
+
+				int i = 0;
+				foreach (XElement entry in xtbl.Elements()) {
+					// FUTURE: Entries by unicode number instead of char
+					if (entry.Name == "Char") {
+						table[i, 0] = entry.Attribute("original").Value[0];
+						table[i, 1] = entry.Attribute("new").Value[0];
+					}
+
+					i++;
+				}
+
+				this.tables[name] = table;
+			}
+
+			// Get special chars
+			XElement xSpecialChars = root.Element("SpecialChars");
+			this.ellipsis = xSpecialChars.Element("Ellipsis").Value;
+			this.quotes = new char[2] { 
+				xSpecialChars.Element("QuoteOpen").Value[0],
+				xSpecialChars.Element("QuoteClose").Value[0]
+			 };
+		}
+	}
+}
+
---
+++ FileManager.cs
@@ -0,0 +1,164 @@
+//-----------------------------------------------------------------------
+// <copyright file="FileManager.cs" company="none">
+// Copyright (C) 2013
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by 
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful, 
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details. 
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see "http://www.gnu.org/licenses/". 
+// </copyright>
+// <author>pleoNeX</author>
+// <email>benito356@gmail.com</email>
+// <date>22/09/2013</date>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Mono.Addins;
+
+namespace Libgame
+{
+	public class FileManager
+	{
+		private static FileManager Instance;
+		private XDocument xmlGame;
+
+		private FileManager(FileContainer root, XDocument xmlGame)
+		{
+			this.Root = root;
+			this.xmlGame = xmlGame;
+		}
+
+		public FileContainer Root {
+			get;
+			private set;
+		}
+
+		public static FileManager GetInstance()
+		 {
+			if (Instance == null)
+				throw new Exception("The class has not been initalized.");
+			return Instance;
+		}
+
+		public static void Initialize(FileContainer rootDir, XDocument xmlGame)
+		{
+			Instance = new FileManager(rootDir, xmlGame);
+		}
+
+		public GameFile RescueFile(string gameFilePath)
+		{
+			XElement fileInfo = this.GetFileInfo(gameFilePath);
+			if (fileInfo != null)
+				return this.RescueFileInfo(gameFilePath, fileInfo);
+			else
+				return this.RescueFileNoInfo(gameFilePath);
+		}
+
+		private GameFile RescueFileNoInfo(string gameFilePath)
+		{
+			// 1.- Gets dependencies
+			// Since no info of dependencies is given, it will search them in two steps.
+			List<GameFile> depends = new List<GameFile>();
+
+			// 1.1.- Gets dependencies to get file data.
+			// It will be the previous GameFile that contains that file.
+			// Reading that file it's expected to get file data.
+			string prevContainer = gameFilePath.GetPreviousPath();
+			if (!string.IsNullOrEmpty(prevContainer)) {
+				GameFile dependency = this.RescueFile(prevContainer);
+				if (dependency != null) {
+					dependency.Format.Read();
+					depends.Add(dependency);
+				}
+			}
+
+			// We should be able to get the file now
+			FileContainer searchFile = this.Root.SearchFile(gameFilePath);
+			GameFile file =  searchFile as GameFile;
+			// If we're trying to get the dependency and found a folder, pass its the "dependency"
+			if (file == null && searchFile is GameFolder) {
+				if (depends.Count > 0)
+					return depends[0];
+				else
+					return null;	// Folder without dependencies
+			} else if (file == null) {
+				throw new Exception("File not found.");
+			}
+
+			// 1.2.- Gets dependencies to be able to parse data.
+			// It will try to guess the file type using FormatValidation classes.
+			// If one of the matches, it will provide the dependencies.
+			foreach (TypeExtensionNode node in AddinManager.GetExtensionNodes(typeof(FormatValidation))) {
+				FormatValidation validation = (FormatValidation)node.CreateInstance();
+				validation.AutosetFormat = true;	// If it matches set format to the file.
+				validation.RunTests(file);
+
+				if (validation.Result) {
+					foreach (string dependencyPath in validation.Dependencies) {
+						GameFile dependency = this.RescueFile(dependencyPath);
+						depends.Add(dependency);
+						dependency.Format.Read();
+					}
+					break;
+				}
+			}
+
+			// Set dependencies
+			file.AddDependencies(depends.ToArray());
+
+			return file;
+		}
+
+		private GameFile RescueFileInfo(string gameFilePath, XElement fileInfo)
+		{
+			// Resolve dependencies
+			List<GameFile> depends = new List<GameFile>();
+
+			foreach (XElement xmlDepend in fileInfo.Elements("DependsOn")) {
+				GameFile dependency = this.RescueFile(xmlDepend.Value);
+				depends.Add(dependency);
+				dependency.Format.Read();
+			}
+
+			// Get file
+			FileContainer searchFile = this.Root.SearchFile(gameFilePath);
+			GameFile file =  searchFile as GameFile;
+			if (file == null) {
+				throw new Exception("File not found.");
+			}
+
+			// Add dependencies
+			file.AddDependencies(depends.ToArray());
+
+			// Get type of dependency from info
+			Type t = Type.GetType(fileInfo.Element("Type").Value, true, false);
+
+			// Set type
+			if (file.Format == null)
+				file.SetFormat(t);
+
+			return file;
+		}
+
+		private XElement GetFileInfo(string path)
+		{
+			XElement files = xmlGame.Root.Element("Files");
+			foreach (XElement fileInfo in files.Elements("FileInfo")) {
+				if (fileInfo.Element("Path").Value == path)
+					return fileInfo;
+			}
+
+			return null;
+		}
+	}
+}
+
