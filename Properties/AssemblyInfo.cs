1cc6032fe5cfe1b04ada20b6fc92e60d0f9ab340
pleonex <benito356@gmail.com>
2014-01-15 11:37:50 +0100

    libgame is now the core of the addin manager
    Libgame and its plugins (addins) can be used by external programs to work easily with games.
    Now there is not any dependency with modime.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,27 @@
+//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="none">
+// Copyright (C) 2014
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by 
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful, 
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details. 
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see "http://www.gnu.org/licenses/". 
+// </copyright>
+// <author>pleoNeX</author>
+// <email>benito356@gmail.com</email>
+// <date>15/01/2014</date>
+//-----------------------------------------------------------------------
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using Mono.Addins;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -8,7 +30,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("pleonex 2013")]
+[assembly: AssemblyCopyright("pleonex 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
@@ -20,3 +42,4 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+[assembly:AddinRoot("libgame", "0.3")]
