a2ee076042f373a283a850c100591eace104dbff
Benito Palacios Sánchez <benito356@gmail.com>
2014-12-16 18:27:51 +0100

    Bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -36,10 +36,10 @@ using Mono.Addins;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.4.*")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-[assembly:AddinRoot("libgame", "0.3")]
+[assembly:AddinRoot("libgame", "0.4")]
