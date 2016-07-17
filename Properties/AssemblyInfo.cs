621aa49075d91740ac1db292a15a5f533ce32688
Benito Palacios Sánchez <benito356@gmail.com>
2016-07-18 01:56:52 +0200

    Bump version to break compatibility

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -26,20 +26,20 @@ using Mono.Addins;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle("libgame")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ROM Hacking library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("pleonex 2015")]
+[assembly: AssemblyCopyright("Benito Palacios Sanchez (pleonex) 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("0.5.*")]
+[assembly: AssemblyVersion("1.0.*")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-[assembly:AddinRoot("libgame", "0.5")]
+[assembly:AddinRoot("libgame", "1.0")]
