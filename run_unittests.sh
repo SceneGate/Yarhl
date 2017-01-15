c545c7fcef8eb11d35c5433432dd5cfe3e2326ae
Benito Palacios Sanchez <benito356@gmail.com>
2017-01-15 20:11:27 +0100

    :arrow_up: NUnit

--- run_unittests.sh
+++ run_unittests.sh
@@ -3,4 +3,4 @@
 xbuild /v:minimal libgame.sln
 if [ $? -ne 0 ] ; then exit 3; fi
 
-mono testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe libgame.UnitTests/bin/Debug/libgame.UnitTests.dll $@
+mono testrunner/NUnit.ConsoleRunner.*/tools/nunit3-console.exe libgame.UnitTests/bin/Debug/libgame.UnitTests.dll $@
