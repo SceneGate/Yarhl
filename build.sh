30e1c8fd6725f49d72180eff544d13df024ec463
Benito Palacios Sanchez <benito356@gmail.com>
2017-11-01 16:00:48 +0100

    :green_heart: Fix missing AppVeyor property and old nuget in Travis

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 msbuild /p:Configuration=Release mono-addins/Mono.Addins/Mono.Addins.csproj
 
 if [[ "$DOTNETCORE" -ne "1" ]]; then
-    nuget restore
+    # Only newer nuget versions support restoring .NET SDK solutions
+    # so if possible we try with dotnet that will work for sure.
+    command -v dotnet >/dev/null 2>&1 && dotnet restore || nuget restore
     msbuild /p:TargetFrameworks=net47
 else
     dotnet restore
