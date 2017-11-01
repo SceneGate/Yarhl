3b66fc4935d550519bd1e4e6a1aff12c0d07fbb0
Benito Palacios Sanchez <benito356@gmail.com>
2017-11-01 15:26:51 +0100

    :green_heart: Remove dotnet dependency in Travis and fix test detection in AppVeyor

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 # Compile Mono.Addins with custom changes
 msbuild /p:Configuration=Release mono-addins/Mono.Addins/Mono.Addins.csproj
 
-# Restore dependencies
-dotnet restore
-
 if [[ "$DOTNETCORE" -ne "1" ]]; then
+    nuget restore
     msbuild /p:TargetFrameworks=net47
 else
+    dotnet restore
     dotnet build -f netcoreapp2.0
 fi
