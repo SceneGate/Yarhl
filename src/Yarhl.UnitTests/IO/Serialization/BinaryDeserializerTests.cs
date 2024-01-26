e8265b5382f60cee4f633e51cf3fff34c4a4d09d
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-01-26 13:06:49 +0100

    :shirt: Extract binary deserializer into class

--- src/Yarhl.UnitTests/IO/Serialization/BinaryDeserializerTests.cs
+++ src/Yarhl.UnitTests/IO/Serialization/BinaryDeserializerTests.cs
@@ -1,290 +1,295 @@
 ﻿namespace Yarhl.UnitTests.IO.Serialization;
 
 using System;
-using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 using Yarhl.IO;
 using Yarhl.IO.Serialization;
 using Yarhl.IO.Serialization.Attributes;
 
 [TestFixture]
-public class BinarySerializerTests
+public class BinaryDeserializerTests
 {
     [Test]
-    public void SerializeMultipleProperties()
+    public void DeserializeFullObject()
     {
-        var obj = new ComplexObject {
-            IntegerValue = 1,
-            LongValue = 2,
-            IgnoredIntegerValue = 3,
-            AnotherIntegerValue = 4,
-        };
-
-        byte[] expected = {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x04, 0x00, 0x00, 0x00,
+            0x03, 0x00, 0x00, 0x00,
+        };
+        var expected = new ComplexObject {
+            IntegerValue = 1,
+            LongValue = 2L,
+            IgnoredIntegerValue = 0,
+            AnotherIntegerValue = 3,
         };
 
-        AssertSerialization(obj, expected);
+        AssertDeserialization(data, expected);
     }
 
     [Test]
-    public void SerializeNestedObject()
+    public void DeserializeNestedObject()
     {
-        var obj = new NestedObject() {
+        byte[] data = {
+            0x0A, 0x00, 0x00, 0x00,
+            0x01, 0x00, 0x00, 0x00,
+            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x03, 0x00, 0x00, 0x00,
+            0x14, 0x00, 0x00, 0x00,
+        };
+        var expected = new NestedObject {
             IntegerValue = 10,
             ComplexValue = new ComplexObject {
                 IntegerValue = 1,
-                LongValue = 2,
-                IgnoredIntegerValue = 3,
-                AnotherIntegerValue = 4,
+                LongValue = 2L,
+                IgnoredIntegerValue = 0,
+                AnotherIntegerValue = 3,
             },
             AnotherIntegerValue = 20,
         };
 
-        byte[] expected = {
-            0x0A, 0x00, 0x00, 0x00,
-            0x01, 0x00, 0x00, 0x00,
-            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x04, 0x00, 0x00, 0x00,
-            0x14, 0x00, 0x00, 0x00,
-        };
-
-        AssertSerialization(obj, expected);
+        AssertDeserialization(data, expected);
     }
 
     [Test]
-    public void SerializeBooleanType()
+    public void DeserializeBoolean()
     {
-        var obj = new ObjectWithDefaultBooleanAttribute() {
-            IntegerValue = 1,
-            BooleanValue = false,
-            IgnoredIntegerValue = 3,
-            AnotherIntegerValue = 4,
-        };
-
-        byte[] expected = {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00,
-            0x04, 0x00, 0x00, 0x00,
+            0x03, 0x00, 0x00, 0x00,
         };
 
-        AssertSerialization(obj, expected);
-    }
-
-    [Test]
-    public void SerializeBooleanWithCustomFalseValue()
-    {
-        var obj = new ObjectWithCustomBooleanAttribute() {
+        var expected = new ObjectWithDefaultBooleanAttribute {
             IntegerValue = 1,
             BooleanValue = false,
-            IgnoredIntegerValue = 5,
-            AnotherIntegerValue = 4,
+            IgnoredIntegerValue = 0,
+            AnotherIntegerValue = 3,
         };
 
-        byte[] expected = {
-            0x01, 0x00, 0x00, 0x00,
-            0x66, 0x61, 0x6C, 0x73, 0x65, 0x00, // "false"
-            0x04, 0x00, 0x00, 0x00,
-        };
-
-        AssertSerialization(obj, expected);
+        AssertDeserialization(data, expected);
     }
 
     [Test]
-    public void TrySerializeBooleanWithoutAttributeThrowsException()
+    public void DeserializeBooleanWithCustomTrueValue()
     {
-        var obj = new ObjectWithoutBooleanAttribute() {
+        byte[] data = {
+            0x01, 0x00, 0x00, 0x00,
+            0x74, 0x72, 0x75, 0x65, 0x00, // "true"
+            0x03, 0x00, 0x00, 0x00,
+        };
+
+        var expected = new ObjectWithCustomBooleanAttribute {
             IntegerValue = 1,
             BooleanValue = true,
-            IgnoredIntegerValue = 3,
-            AnotherIntegerValue = 4,
-        };
-
-        using var stream = new DataStream();
-        var serializer = new BinarySerializer(stream);
-
-        _ = Assert.Throws<FormatException>(() => serializer.Serialize(obj));
-    }
-
-    [Test]
-    public void SerializeInt24()
-    {
-        var obj = new ObjectWithInt24 {
-            Int24Value = 0x7F_FC0FFE,
-        };
-
-        byte[] expected = {
-            0xFE, 0x0F, 0xFC,
-        };
-
-        AssertSerialization(obj, expected);
-    }
-
-    [Test]
-    public void SerializeStringWithoutAttributeUsesDefaultWriterSettings()
-    {
-        var obj = new ObjectWithoutStringAttribute {
-            IntegerValue = 1,
-            StringValue = "あア",
-            IgnoredIntegerValue = 2,
+            IgnoredIntegerValue = 0,
             AnotherIntegerValue = 3,
         };
 
-        byte[] expected = {
+        AssertDeserialization(data, expected);
+    }
+
+    [Test]
+    public void TryDeserializeBooleanWithoutAttributeThrowsException()
+    {
+        byte[] data = {
+            0x01, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x03, 0x00, 0x00, 0x00,
+        };
+
+        var stream = new DataStream();
+        stream.Write(data);
+
+        stream.Position = 0;
+        var deserializer = new BinaryDeserializer(stream);
+        Assert.That(
+            () => deserializer.Deserialize<ObjectWithoutBooleanAttribute>(),
+            Throws.InstanceOf<FormatException>());
+    }
+
+    [Test]
+    public void DeserializeInt24()
+    {
+        byte[] data = {
+            0x01, 0x00, 0x00,
+        };
+
+        var expected = new ObjectWithInt24 {
+            Int24Value = 1,
+        };
+
+        AssertDeserialization(data, expected);
+    }
+
+    [Test]
+    public void DeserializeStringWithoutAttributeUsesDefaultReaderSettings()
+    {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0xE3, 0x81, 0x82, 0xE3, 0x82, 0xA2, 0x00,
             0x03, 0x00, 0x00, 0x00,
         };
 
-        AssertSerialization(obj, expected);
-    }
-
-    [Test]
-    public void SerializeStringWithDefaultAttributeUsesDefaultWriterSettings()
-    {
-        var obj = new ObjectWithDefaultStringAttribute() {
+        var expected = new ObjectWithoutStringAttribute {
             IntegerValue = 1,
             StringValue = "あア",
-            IgnoredIntegerValue = 2,
+            IgnoredIntegerValue = 0,
             AnotherIntegerValue = 3,
         };
 
-        byte[] expected = {
+        AssertDeserialization(data, expected);
+    }
+
+    [Test]
+    public void DeserializeStringWithDefaultAttributeUsesDefaultReaderSettings()
+    {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0xE3, 0x81, 0x82, 0xE3, 0x82, 0xA2, 0x00,
             0x03, 0x00, 0x00, 0x00,
         };
 
-        AssertSerialization(obj, expected);
+        var expected = new ObjectWithDefaultStringAttribute {
+            IntegerValue = 1,
+            StringValue = "あア",
+            IgnoredIntegerValue = 0,
+            AnotherIntegerValue = 3,
+        };
+
+        AssertDeserialization(data, expected);
     }
 
     [Test]
-    public void SerializeStringWithSizeType()
+    public void DeserializeStringWithSizeType()
     {
-        var obj = new ObjectWithCustomStringAttributeSizeUshort() {
-            IntegerValue = 1,
-            StringValue = "あ",
-            IgnoredIntegerValue = 2,
-            AnotherIntegerValue = 4,
-        };
-
-        byte[] expected = {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0x03, 0x00, 0xE3, 0x81, 0x82,
             0x04, 0x00, 0x00, 0x00,
         };
 
-        AssertSerialization(obj, expected);
-    }
-
-    [Test]
-    public void SerializeStringWithFixedSize()
-    {
-        var obj = new ObjectWithCustomStringAttributeFixedSize() {
+        var expected = new ObjectWithCustomStringAttributeSizeUshort {
             IntegerValue = 1,
             StringValue = "あ",
-            IgnoredIntegerValue = 2,
+            IgnoredIntegerValue = 0,
             AnotherIntegerValue = 4,
         };
 
-        byte[] expected = {
+        AssertDeserialization(data, expected);
+    }
+
+    [Test]
+    public void DeserializeStringWithFixedSize()
+    {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0xE3, 0x81, 0x82,
-            0x04, 0x00, 0x00, 0x00,
+            0x03, 0x00, 0x00, 0x00,
         };
 
-        AssertSerialization(obj, expected);
+        var expected = new ObjectWithCustomStringAttributeFixedSize {
+            IntegerValue = 1,
+            StringValue = "あ",
+            IgnoredIntegerValue = 0,
+            AnotherIntegerValue = 3,
+        };
+
+        AssertDeserialization(data, expected);
     }
 
     [Test]
-    public void SerializeStringWithDifferentEncoding()
+    public void DeserializeStringWithDifferentEncoding()
     {
-        var obj = new ObjectWithCustomStringAttributeCustomEncoding() {
-            IntegerValue = 1,
-            StringValue = "あア",
-            IgnoredIntegerValue = 2,
-            AnotherIntegerValue = 4,
-        };
-
-        byte[] expected = {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0x82, 0xA0, 0x83, 0x41, 0x00,
-            0x04, 0x00, 0x00, 0x00,
+            0x03, 0x00, 0x00, 0x00,
         };
 
-        AssertSerialization(obj, expected);
-    }
-
-    [Test]
-    public void TrySerializeStringWithUnknownEncodingThrowsException()
-    {
-        var obj = new ObjectWithCustomStringAttributeUnknownEncoding() {
+        var expected = new ObjectWithCustomStringAttributeCustomEncoding {
             IntegerValue = 1,
             StringValue = "あア",
-            IgnoredIntegerValue = 2,
-            AnotherIntegerValue = 4,
+            IgnoredIntegerValue = 0,
+            AnotherIntegerValue = 3,
         };
 
-        using var stream = new DataStream();
-        var serializer = new BinarySerializer(stream);
-
-        _ = Assert.Throws<NotSupportedException>(() => serializer.Serialize(obj));
+        AssertDeserialization(data, expected);
     }
 
     [Test]
-    public void SerializeObjectWithSpecificEndianness()
+    public void TryDeserializeStringWithUnknownEncodingThrowsException()
     {
-        var obj = new ObjectWithForcedEndianness() {
-            LittleEndianInteger = 1,
-            BigEndianInteger = 2,
-            DefaultEndianInteger = 3,
+        byte[] data = {
+            0x01, 0x00, 0x00, 0x00,
+            0x82, 0xA0, 0x83, 0x41, 0x00,
+            0x03, 0x00, 0x00, 0x00,
         };
 
-        byte[] expected = {
+        var stream = new DataStream();
+        stream.Write(data);
+
+        stream.Position = 0;
+        var deserializer = new BinaryDeserializer(stream);
+
+        Assert.That(
+            () => deserializer.Deserialize<ObjectWithCustomStringAttributeUnknownEncoding>(),
+            Throws.InstanceOf<NotSupportedException>());
+    }
+
+    [Test]
+    public void DeserializeObjectWithSpecificEndianness()
+    {
+        byte[] data = {
             0x01, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x02,
             0x03, 0x00, 0x00, 0x00,
         };
 
-        AssertSerialization(obj, expected);
+        var expected = new ObjectWithForcedEndianness {
+            LittleEndianInteger = 1,
+            BigEndianInteger = 2,
+            DefaultEndianInteger = 3,
+        };
+
+        AssertDeserialization(data, expected);
     }
 
     [Test]
-    public void SerializeEnum()
+    public void DeserializeEnum()
     {
-        var obj = new ObjectWithEnum() {
-            EnumValue = Enum1.Value2,
-        };
+        byte[] data = { 0x01 };
 
-        byte[] expected = { 0x01 };
+        var expected = new ObjectWithEnum { EnumValue = Enum1.Value2 };
 
-        AssertSerialization(obj, expected);
+        AssertDeserialization(data, expected);
     }
 
-    private static void AssertSerialization<T>(T obj, byte[] expected)
+    [Test]
+    public void TryDeserializeNullableThrowsException()
     {
-        using var stream = new DataStream();
-        var serializer = new BinarySerializer(stream);
+        var stream = new DataStream();
+        stream.Write(new byte[4]);
 
-        serializer.Serialize(obj);
+        stream.Position = 0;
+        var deserializer = new BinaryDeserializer(stream);
 
-        AssertBinary(stream, expected);
+        Assert.That(
+            () => deserializer.Deserialize<ObjectWithNullable>(),
+            Throws.InstanceOf<FormatException>());
     }
 
-    private static void AssertBinary(DataStream actual, byte[] expected)
+    private static void AssertDeserialization<T>(byte[] data, T expected)
     {
-        Assert.That(expected.Length, Is.EqualTo(actual.Length));
+        var stream = new DataStream();
+        stream.Write(data);
 
-        byte[] actualData = new byte[expected.Length];
-        Assert.Multiple(() => {
-            actual.Position = 0;
-            int read = actual.Read(actualData);
+        stream.Position = 0;
+        var deserializer = new BinaryDeserializer(stream);
+        T obj = deserializer.Deserialize<T>();
 
-            Assert.That(read, Is.EqualTo(expected.Length));
-            Assert.That(actualData, Is.EquivalentTo(actualData));
-        });
+        _ = obj.Should().BeEquivalentTo(expected);
     }
 
     [Yarhl.IO.Serialization.Attributes.Serializable]
@@ -342,7 +347,7 @@ public class BinarySerializerTests
     {
         public int IntegerValue { get; set; }
 
-        [BinaryBoolean(WriteAs = typeof(string), TrueValue = "true", FalseValue = "false")]
+        [BinaryBoolean(ReadAs = typeof(string), TrueValue = "true")]
         public bool BooleanValue { get; set; }
 
         [BinaryIgnore]
@@ -446,17 +451,10 @@ public class BinarySerializerTests
         public int DefaultEndianInteger { get; set; }
     }
 
-    private enum Enum1
-    {
-        Value1,
-        Value2,
-        Value3,
-    }
-
     [Yarhl.IO.Serialization.Attributes.Serializable]
     private class ObjectWithEnum
     {
-        [BinaryEnum(WriteAs = typeof(byte))]
+        [BinaryEnum(ReadAs = typeof(byte))]
         public Enum1 EnumValue { get; set; }
     }
 
@@ -465,5 +463,18 @@ public class BinarySerializerTests
     {
         [BinaryInt24]
         public int Int24Value { get; set; }
+    }
+
+    [Yarhl.IO.Serialization.Attributes.Serializable]
+    private class ObjectWithNullable
+    {
+        public int? NullValue { get; set; }
+    }
+
+    private enum Enum1
+    {
+        Value1,
+        Value2,
+        Value3,
     }
 }
---
+++ src/Yarhl/IO/Serialization/BinaryDeserializer.cs
@@ -0,0 +1,160 @@
+﻿namespace Yarhl.IO.Serialization;
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Yarhl.IO.Serialization.Attributes;
+
+/// <summary>
+/// Binary deserialization of objects based on their attributes. Equivalent of
+/// converting a binary format into an object.
+/// </summary>
+public class BinaryDeserializer
+{
+    private readonly DataReader reader;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BinaryDeserializer"/> class.
+    /// </summary>
+    /// <param name="stream">The stream to read from.</param>
+    public BinaryDeserializer(Stream stream)
+    {
+        reader = new DataReader(stream);
+    }
+
+    /// <summary>
+    /// Gets or sets the default endianness for the deserialization.
+    /// </summary>
+    public EndiannessMode DefaultEndianness { get; set; }
+
+    /// <summary>
+    /// Deserialize an object from the binary data of the stream.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to deserialize.</typeparam>
+    /// <param name="stream">The stream to read from.</param>
+    /// <returns>A new object deserialized.</returns>
+    public static T Deserialize<T>(Stream stream)
+    {
+        return new BinaryDeserializer(stream).Deserialize<T>();
+    }
+
+    /// <summary>
+    /// Deserialize an object from the binary data of the stream.
+    /// </summary>
+    /// <param name="stream">The stream to read from.</param>
+    /// <param name="objType">The type of the object to deserialize.</param>
+    /// <returns>A new object deserialized.</returns>
+    public static object Deserialize(Stream stream, Type objType)
+    {
+        return new BinaryDeserializer(stream).Deserialize(objType);
+    }
+
+    /// <summary>
+    /// Deserialize an object from the binary data of the stream.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to deserialize.</typeparam>
+    /// <returns>A new object deserialized.</returns>
+    public T Deserialize<T>()
+    {
+        return (T)Deserialize(typeof(T));
+    }
+
+    /// <summary>
+    /// Deserialize an object from the binary data of the stream.
+    /// </summary>
+    /// <param name="objType">The type of the object to deserialize.</param>
+    /// <returns>A new object deserialized.</returns>
+    public object Deserialize(Type objType)
+    {
+        // It returns null for Nullable<T>, but as that is a class and
+        // it won't have the serializable attribute, it will throw an
+        // unsupported exception before. So this can't be null at this point.
+        object obj = Activator.CreateInstance(objType)!;
+
+        PropertyInfo[] properties = objType.GetProperties(
+            BindingFlags.DeclaredOnly |
+            BindingFlags.Public |
+            BindingFlags.Instance);
+
+        foreach (PropertyInfo property in properties) {
+            bool ignore = Attribute.IsDefined(property, typeof(BinaryIgnoreAttribute));
+            if (ignore) {
+                continue;
+            }
+
+            object propertyValue = DeserializePropertyValue(property);
+            property.SetValue(obj, propertyValue);
+        }
+
+        return obj;
+    }
+
+    private object DeserializePropertyValue(PropertyInfo property)
+    {
+        reader.Endianness = DefaultEndianness;
+        var endiannessAttr = property.GetCustomAttribute<BinaryForceEndiannessAttribute>();
+        if (endiannessAttr is not null) {
+            reader.Endianness = endiannessAttr.Mode;
+        }
+
+        if (property.PropertyType.IsPrimitive) {
+            return DeserializePrimitiveField(property);
+        } else if (property.PropertyType.IsEnum) {
+            return DeserializeEnumField(property);
+        } else if (property.PropertyType == typeof(string)) {
+            return DeserializeStringField(property);
+        } else {
+            return Deserialize(property.PropertyType);
+        }
+    }
+
+    private object DeserializePrimitiveField(PropertyInfo property)
+    {
+        if (property.PropertyType == typeof(bool)) {
+            if (property.GetCustomAttribute<BinaryBooleanAttribute>() is not { } boolAttr) {
+                throw new FormatException("Properties of type 'bool' must have the attribute BinaryBoolean");
+            }
+
+            object value = reader.ReadByType(boolAttr!.ReadAs);
+            return value.Equals(boolAttr.TrueValue);
+        }
+
+        if (property.PropertyType == typeof(int) && Attribute.IsDefined(property, typeof(BinaryInt24Attribute))) {
+            return reader.ReadInt24();
+        }
+
+        return reader.ReadByType(property.PropertyType);
+    }
+
+    private object DeserializeEnumField(PropertyInfo property)
+    {
+        if (property.GetCustomAttribute<BinaryEnumAttribute>() is not { } enumAttr) {
+            throw new FormatException("Missing BinaryEnum attribute in property");
+        }
+
+        object value = reader.ReadByType(enumAttr!.ReadAs);
+        return Enum.ToObject(property.PropertyType, value);
+    }
+
+    private string DeserializeStringField(PropertyInfo property)
+    {
+        if (property.GetCustomAttribute<BinaryStringAttribute>() is not { } stringAttr) {
+            // Use default settings if not specified.
+            return reader.ReadString();
+        }
+
+        Encoding? encoding = null;
+        if (stringAttr!.CodePage != -1) {
+            encoding = Encoding.GetEncoding(stringAttr.CodePage);
+        }
+
+        if (stringAttr.SizeType is null) {
+            return (stringAttr.FixedSize == -1)
+                ? reader.ReadString(encoding)
+                : reader.ReadString(stringAttr.FixedSize, encoding);
+        }
+
+        return reader.ReadString(stringAttr.SizeType, encoding);
+    }
+}
